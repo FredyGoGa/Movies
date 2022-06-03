77e39f1f7a59637538196c4912013187cd5d08cf
Fredy Gonzalez <fredylingling.fgg6@gmail.com>
2022-06-02 20:03:16 -0500

    se instalo material ui correctamente, tener en cuenta la version de instalacion para no tener fallas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Product from './components/Product';
 
 function App() {
   return (
     <div className="App">
       <h1>Aca comienza la tienda virtual de pochita</h1>
+      <Product/>
     </div>
   );
 }
