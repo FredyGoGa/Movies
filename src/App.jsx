a0eb72a1e83fa6b3957a043bb298f86bd34e8d36
Fredy Gonzalez <fredylingling.fgg6@gmail.com>
2022-07-25 12:00:26 -0500

    se reemplazo Switch por por Routes por ser la v6, pendiente corregir las vistas ya que no muestra el contenido

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
 import { MoviesGrid } from "./components/MoviesGrid";
 import styles from "./App.module.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
 
 export function App() {
   return (
-    <div>
+    <Router>
       <header>
-        <h1 className={styles.title}>Movies</h1>
+       
+          {/* <h1 className={styles.title}>Movies</h1> */}
+           <Link to="/">Home</Link>
+           <hr/>
+           <Link to="/movie">Movie</Link>
+           <hr/>
+      
       </header>
       <main>
-        <MoviesGrid />
+        <Routes>
+
+         <Route exact path="/movie">Moviedfgadfgdafg</Route>
+        <Route  path="/">home</Route>
+       
+          
+            
+          
+        </Routes>
       </main>
-    </div>
+    </Router>
   );
+ 
 }
