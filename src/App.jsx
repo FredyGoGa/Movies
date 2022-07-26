23f69f3a953971893d3c1d9b063e0ecc336b2f10
Fredy Gonzalez <fredylingling.fgg6@gmail.com>
2022-07-26 10:03:51 -0500

    se corrigio la sintaxis de router, en exact path, faltaba agregar element={<MoviesGrid}.ruteo funcionando

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,32 @@
 import { MoviesGrid } from "./components/MoviesGrid";
 import styles from "./App.module.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-
+import { MovieDetails} from "./pages/MovieDetails"
 
 export function App() {
   return (
     <Router>
       <header>
-       
-          {/* <h1 className={styles.title}>Movies</h1> */}
+
+           <h1 className={styles.title}>Movies</h1>
            <Link to="/">Home</Link>
            <hr/>
-           <Link to="/movie">Movie</Link>
+           <Link to="/movies">Movies</Link>
            <hr/>
-      
+
       </header>
       <main>
         <Routes>
 
-         <Route exact path="/movie">Moviedfgadfgdafg</Route>
-        <Route  path="/">home</Route>
-       
-          
-            
-          
+         <Route exact path="/movies" element={<MoviesGrid/>}/>
+         {/* <Route path="/"elemn>home</Route> */}
+
+
+
+
         </Routes>
       </main>
     </Router>
   );
- 
+
 }
