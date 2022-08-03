25984f0d62876e5e3c966adbd9b3231db11ad2cf
Fredy Gonzalez <fredylingling.fgg6@gmail.com>
2022-08-02 19:49:44 -0500

    listo para conectar a API

--- src/components/MoviesGrid.jsx
+++ src/components/MoviesGrid.jsx
@@ -3,6 +3,9 @@ import movies from "./movies.json";
 import styles from "./MoviesGrid.module.css";
 
 export function MoviesGrid() {
+
+  
+
   return (
     <ul className={styles.moviesGrid}> 
       {movies.map((movie) => (
