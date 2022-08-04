60eea5743b894cd52fca252066e785a6ad981140
Fredy Gonzalez <fredylingling.fgg6@gmail.com>
2022-08-04 17:15:50 -0500

    useEffect en MoviesGrid y fetch para hacer las peticiones en donde se incorporor el token de acceso a la api. se retiro let movies y se recorrio el array mostrondo en consola el .js de la movies

--- src/components/MoviesGrid.jsx
+++ src/components/MoviesGrid.jsx
@@ -1,13 +1,27 @@
+import { useEffect } from "react";
 import { MovieCard } from "./MovieCard";
-import movies from "./movies.json";
+// import movies from "./movies.json";
 import styles from "./MoviesGrid.module.css";
 
 export function MoviesGrid() {
+  let movies = [];
+  useEffect(() =>{
+    fetch("https://api.themoviedb.org/4/discover/movie",{
+      headers:{
+        Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5N2Y3NmM3NGI3M2Q0Yjg0ZmIxMzdjNDQ4NzEwZmQ1ZCIsInN1YiI6IjYyZWMwNWIyYWY0MzI0MDA1YzFhYWNkMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.16tv0zCbnSjhNKdKV77BX-EMlnWHOO8Bw5qEXjNBsXo",
+        "Content-Type": "application/json;charset=utf-8",
 
-  
+      },
+    }).then(result => result.json())
+    .then((data) => {
+      movies = data.results;
+      console.log(movies);
+    })
+});
 
   return (
-    <ul className={styles.moviesGrid}> 
+    <ul className={styles.moviesGrid}>
       {movies.map((movie) => (
         <MovieCard key={movie.id} movie={movie} />
       ))}
