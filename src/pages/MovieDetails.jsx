1829802e26c0a1f35fe861d7acc9c7a3a6a99abc
Fredy Gonzalez <fredylingling.fgg6@gmail.com>
2022-08-18 09:19:38 -0500

    instalacion react-icons,se creo el componenete spinner con su repectivo module.css, se utilizo spinner en moviesdetails y moviesgrid,

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,21 +1,30 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import { Spinner} from "../components/Spinner";
 import { get } from "../utils/httpClient";
 import styles from "./MovieDetails.module.css";
 
 export function MovieDetails() {
   const { movieId } = useParams();
+  const [isLoading, setIsLoading] = useState(true);
   const [movie, setMovie] = useState(null);
 
   useEffect(() => {
+    setIsLoading(true);
     get("/movie/" + movieId).then((data) => {
-      setMovie(data);
+    
+    setMovie(data);
+    setIsLoading(false);
+    
+      
     });
   }, [movieId]);
 
-  if (!movie) {
-    return null;
+  if(isLoading){
+    return <Spinner/>;
   }
+
+ 
 
   const imageUrl = "https://image.tmdb.org/t/p/w500" + movie.poster_path;
   return (
@@ -29,10 +38,12 @@ export function MovieDetails() {
         <p className={styles.firstItem}>
           <strong>Title:</strong> {movie.title}
         </p>
+        <br />
         <p>
           <strong>Genres:</strong>{" "}
           {movie.genres.map((genre) => genre.name).join(", ")}
         </p>
+        <br />
         <p>
           <strong>Description:</strong> {movie.overview}
         </p>
