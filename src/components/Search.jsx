ce005290d13ca42c48e5ac8a0c4c49ece5430183
Fredy Gonzalez <fredylingling.fgg6@gmail.com>
2022-08-18 15:24:30 -0500

    uso de useState para recibir parametros en la url como el id de la pelicula, en la v6 de react router useHistory es reemplazado por useNavigate y no se pone .push

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,12 +1,24 @@
 import styles from "./Search.module.css";
 import { FaSearch } from "react-icons/fa";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 
 export function Search(){
+
+  const [searchText, setSearchText] = useState("");
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate("/?search=" + searchText);
+
+  };
     return(
-        <form className={styles.searchContainer}>
-            <div className="styles.searchBox">
-                <input className="styles.searchInput" type="text"/>
-                <button className="styles.searchButton"type="submit"><FaSearch/></button>
+        <form className={styles.searchContainer}onSubmit={handleSubmit}>
+            <div className={styles.searchBox}>
+                <input className={styles.searchInput} type="text" value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}/>
+                <button className={styles.searchButton}type="submit"><FaSearch size={20}/></button>
             </div>
         </form>
     )
